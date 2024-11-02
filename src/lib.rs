e3e597118ac937219ff154e2ba3c45cdde1dc2c1
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2024-11-02 19:16:47 +0330

    :rocket: Init

---
+++ src/lib.rs
@@ -0,0 +1,72 @@
+use std::{future::Future, pin::pin};
+
+use futures::future;
+
+pub use futures::{
+    future::{join, join3, join_all, Either},
+    join,
+};
+
+pub use tokio::{
+    fs::read_to_string,
+    runtime::Runtime,
+    sync::mpsc::{
+        unbounded_channel as channel, UnboundedReceiver as Receiver, UnboundedSender as Sender,
+    },
+    task::{spawn as spawn_task, yield_now, JoinHandle},
+    time::{interval, sleep},
+};
+
+pub use tokio_stream::{
+    iter as stream_from_iter,
+    wrappers::{IntervalStream, UnboundedReceiverStream as ReceiverStream},
+    Stream, StreamExt,
+};
+
+pub fn run<F: Future>(future: F) -> F::Output {
+    let rt = Runtime::new().unwrap();
+    rt.block_on(future)
+}
+
+pub async fn race<A, B, F1, F2>(f1: F1, f2: F2) -> Either<A, B>
+where
+    F1: Future<Output = A>,
+    F2: Future<Output = B>,
+{
+    let f1 = pin!(f1);
+    let f2 = pin!(f2);
+
+    match future::select(f1, f2).await {
+        Either::Left((a, _f2)) => Either::Left(a),
+        Either::Right((b, _f1)) => Either::Right(b),
+    }
+}
+
+pub struct Response(reqwest::Response);
+
+impl Response {
+    pub async fn text(self) -> String {
+        self.0.text().await.unwrap()
+    }
+}
+
+pub async fn get(url: &str) -> Response {
+    Response(reqwest::get(url).await.unwrap())
+}
+
+pub struct Html {
+    inner: scraper::Html,
+}
+
+impl Html {
+    pub fn parse(source: &str) -> Html {
+        Html {
+            inner: scraper::Html::parse_document(source),
+        }
+    }
+    pub fn select_first<'a>(&'a self, selector: &'a str) -> Option<scraper::ElementRef<'a>> {
+        let selector = scraper::Selector::parse(selector).unwrap();
+
+        self.inner.select(&selector).nth(0)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use lib::{get, race, run, Either, Html};
+use std::env::args;
+
+fn main() {
+    println!("Async and Await");
+    let args: Vec<String> = args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: cargo run <url>");
+        return;
+    }
+
+    run(async {
+        let title_future_1 = page_title_for(&args[1]);
+        let title_future_2 = page_title_for(&args[2]);
+
+        let (url, maybe_title) = match race(title_future_1, title_future_2).await {
+            Either::Left(left) => left,
+            Either::Right(right) => right,
+        };
+
+        println!("{url} returned first");
+        match maybe_title {
+            Some(title) => println!("Its page title is: '{title}'"),
+            None => println!("Its title could not be parsed."),
+        }
+    });
+}
+
+async fn page_title_for(url: &str) -> (&str, Option<String>) {
+    let text = get(url).await.text().await;
+
+    let title = Html::parse(&text)
+        .select_first("title")
+        .map(|title_element| title_element.inner_html());
+
+    (url, title)
+}
